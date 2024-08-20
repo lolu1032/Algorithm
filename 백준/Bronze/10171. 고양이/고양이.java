6d5d555ab86272ac880cab2dbb9d732bb64b78e2
Park Sungho <81069048+lolu1032@users.noreply.github.com>
2024-08-20 23:25:31 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("\\    /\\\n");
+        bw.write(" )  ( ')\n");
+        bw.write("(  /  )\n");
+        bw.write(" \\(__)|");
+        bw.flush();
+        bw.close();
+    }
+}
