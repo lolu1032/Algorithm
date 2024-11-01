ab4d0012b19fc06f27f578cdf12ed039d70d06ef
Dal col <81069048+lolu1032@users.noreply.github.com>
2024-11-01 22:00:36 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 212 ms, Memory: 14572 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i <n; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 0; i < n-1; i++) {
+            for(int j = 0; j < n-i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
