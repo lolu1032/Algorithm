d97e6986b3219cf768ef07b15738b620cdcf90d6
Dal col <81069048+lolu1032@users.noreply.github.com>
2024-11-08 21:42:20 +0900

    [Bronze V] Title: 오늘 날짜, Time: 96 ms, Memory: 13064 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,12 @@
+
+import java.time.LocalDate;
+import java.util.*;
+
+import static java.time.LocalTime.now;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        LocalDate day = LocalDate.now();
+        System.out.println(day);
+    }
+}
