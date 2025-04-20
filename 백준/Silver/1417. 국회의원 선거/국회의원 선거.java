b18f28ebb593acddfe71689e38c25fee90d38030
sung <81069048+lolu1032@users.noreply.github.com>
2025-04-20 20:25:17 +0900

    [Silver V] Title: 국회의원 선거, Time: 92 ms, Memory: 12952 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -3,32 +3,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // 인원수
         int n = sc.nextInt();
+        int dazung = sc.nextInt();
         int count = 0;
-        if(n == 1) {
-            System.out.println(count);
-        }else {
-            List<Integer> list = new ArrayList<>();
-            for(int i = 0; i < n; i++) {
-                // 1~n번 투표수 입력
-                list.add(sc.nextInt());
-            }
-    
-            int a = list.get(0);
-            list.remove(0);
-            while (true) {
-                list.sort(Comparator.reverseOrder());
-                if(a > list.get(0)) {
-                    break;
-                }
-                int max = list.get(0);
-                list.set(0,max-1);
-                a++;
-                count++;
-            }
-            System.out.println(count);
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 1; i < n; i++) {
+            queue.add(sc.nextInt());
         }
+
+        while(!queue.isEmpty() && queue.peek() >= dazung) {
+            int max = queue.poll();
+            max--;
+            dazung++;
+            count++;
+
+            queue.add(max);
+        }
+        System.out.println(count);
     }
 }
